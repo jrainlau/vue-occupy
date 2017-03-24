dc9bc5071a497bc84c4430e9e72380bb7c21d7d1
JrainLau <jrainlau@163.com>
2017-03-24 10:32:54 +0800

    Fixed the bug that occupy blocks won’t be replaced by the existing data

--- src/components/index.js
+++ src/components/index.js
@@ -1,24 +1,30 @@
 const install = (Vue) => {
   Vue.directive('occupy', {
     bind (el, binding, vnode) {
-      let _placeHolder = '<div style="'
-      const _defaultConfig = {
-        width: '100%',
-        height: '100%',
-        background: '#eee'
+      if (!binding.value.data) {
+        let _placeHolder = '<div style="'
+        const _defaultConfig = {
+          width: '100%',
+          height: '100%',
+          background: '#eee'
+        }
+        const _configObj = { ..._defaultConfig, ...binding.value.config, transition: 'all .3s' }
+        Object.keys(_configObj).forEach((style) => {
+          _placeHolder += `${style}:${_configObj[style]};`
+        })
+        _placeHolder += '"></div>'
+        el.innerHTML = _placeHolder
+      } else {
+        el.innerHTML = binding.value.data
       }
-      const _configObj = { ..._defaultConfig, ...binding.value.config, transition: 'all .3s' }
-      Object.keys(_configObj).forEach((style) => {
-        _placeHolder += `${style}:${_configObj[style]};`
-      })
-      _placeHolder += '"></div>'
-      el.innerHTML = _placeHolder
     },
     update (el, binding) {
-      el.children[0].style.opacity = 0
-      setTimeout(() => {
-        el.innerHTML = binding.value.data
-      }, 300)
+      if (el.children[0]) {
+        el.children[0].style.opacity = 0
+        setTimeout(() => {
+          el.innerHTML = binding.value.data
+        }, 300)
+      }
     }
   })
 }
